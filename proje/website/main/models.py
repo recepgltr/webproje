73fac33ef8f5e89ec71f32f9553f743b804e5250
ALI EIDOU <167813175+alieidou@users.noreply.github.com>
2025-05-05 10:35:09 +0300

    Update models.py

--- proje/website/main/models.py
+++ proje/website/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publish_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.title
